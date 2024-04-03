bea06d30228fd3b55f55e45fe9f07a626b53921c
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-04-03 11:08:40 -0500

    aggiunte istanze

--- index.php
+++ index.php
@@ -6,10 +6,15 @@ require './Models/Categoria.php';
 $cani = new Categoria('cani','fa-solid fa-dog');
 $gatti = new Categoria('gatti','fa-solid fa-cat');
 
-$prodotto1 = new Prodotto('Palla','10.00','https://picsum.photos/300/200', $cani);
+$prodotto1 = new Prodotto('Palla','$10.00','https://picsum.photos/300/200', $cani);
+$prodotto2 = new Prodotto('Cibo','$15.00','https://picsum.photos/300/200', $gatti);
+$prodotto3 = new Prodotto('Cuccia','$30.00', 'https://picsum.photos/300/200', $cani);
+
 
 $prodotti = [
     $prodotto1,
+    $prodotto2,
+    $prodotto3,
 ]
 
 ?>  
@@ -22,6 +27,8 @@ $prodotti = [
     <title>PHP OOP 2</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body>
     <div class="container">
@@ -33,10 +40,17 @@ $prodotti = [
                 
                 foreach ($prodotti as $prodotto) {
                 ?>
+                <div class="col d-flex justify-content-center">
+                    <div class="card">
+                        <img src="<?= $prodotto->immagine ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5><?= $prodotto->nome ?></h5>
+                            <h6><?= $prodotto->prezzo ?></h6>
+                            <i class="<?= $prodotto->categoria->getCategoria() ?>"></i>
+                        </div>
 
-                    <img src="<?= $prodotto->immagine ?>" alt="...">
-                    <p><?= $prodotto->nome ?></p>
-                    <p><?= $prodotto->prezzo ?></p>
+                    </div>
+                </div>
 
             <?php
             }

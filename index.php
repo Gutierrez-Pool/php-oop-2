b84c3e7624bbaa0b6e317ef7b8c8a7e160dd943d
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-04-03 22:27:57 -0500

    aggiunto style

--- index.php
+++ index.php
@@ -29,26 +29,51 @@ $prodotti = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Pet Shop</h1>
 
-        <div>
-            <div>
+    <style>
+        .card-img-top {
+            position: relative;
+        }
+
+        .fa-solid {
+            position: absolute;
+            top: 8px;
+            right: 8px;
+            background-color: white;
+            border-radius: 50%;
+            padding: 10px;
+        }
+
+
+    </style>
+
+
+</head>
+<body style="background-color: darkslateblue">
+    <div class="container">
+            <h1 class="text-white p-5">
+                Pet Shop
+            </h1>
+        
+            <div class="d-flex justify-content-around">
                 <?php 
                 
                 foreach ($prodotti as $prodotto) {
                 ?>
-                <div class="col d-flex justify-content-center">
+                <div class="col-3 d-flex justify-content-center">
                     <div class="card">
-                        <img src="<?= $prodotto->immagine ?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5><?= $prodotto->nome ?></h5>
-                            <h6><?= $prodotto->prezzo ?></h6>
+                        <div>
+                            <img src="<?= $prodotto->immagine ?>" class="card-img-top" alt="...">
                             <i class="<?= $prodotto->categoria->getCategoria() ?>"></i>
                         </div>
-
+                        <div class="card-body d-flex justify-content-between align-items-center">
+                            <h4>
+                                <?= $prodotto->nome ?>
+                            </h4>
+                            <div>
+                                <?= $prodotto->prezzo ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -56,7 +81,6 @@ $prodotti = [
             }
             ?>
             </div>
-        </div>
     </div>
     
 
